90e59ec053f09ebbd0a44d76f9c04148eef1f31d
rawanessam <rawanessam34@gmail.com>
2025-07-21 17:43:41 +0400

    minor fix

--- backend/src/api/ticket_db.py
+++ backend/src/api/ticket_db.py
@@ -1,8 +1,20 @@
 import json
 import os
+from dotenv import load_dotenv
+import os
+from pathlib import Path
 
-DB_PATH = "ticket_db.json"
+### Script to save ticket data an LLM reponse to .json db
+load_dotenv()
 
+config = os.getenv("CONFIG_FILE")
+src_path = os.getcwd()
+### Process config file
+config_dict = json.loads(open(f"{config}").read())
+
+DB_PATH = config_dict["DB_path"]
+
+#DB_PATH = "DB.json"
 def save_ticket(ticket_id: str, user_data: dict, llm_response: dict):
     """
     Save ticket data and LLM response to a local JSON file.
@@ -19,8 +31,13 @@ def save_ticket(ticket_id: str, user_data: dict, llm_response: dict):
             try:
                 db = json.load(f)
             except json.JSONDecodeError:
+    
                 db = []
     else:
+        file_path = Path(DB_PATH)
+        dir_path = file_path.parent
+        if not os.path.exists(dir_path):
+            dir_path.mkdir(parents=True)
         db = []
 
     # Append new entry
